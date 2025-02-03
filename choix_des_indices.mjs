81d37791aa1a9dc12256b105173ae8c3a31fd273
Anais5s <anaisboisson@gmail.com>
2025-02-03 09:21:31 +0100

    demander indices

---
+++ choix_des_indices.mjs
@@ -0,0 +1,14 @@
+import { askQuestion } from './selection_mot_mystere.mjs';
+
+var indices = [];
+
+async function demander_indices(mot,indice){
+    let indices=[]
+    for (let i=0; i<noms.length;i++){
+        if (i!=indice){
+            console.log("A toi ", noms[i]);
+            indices.push(await askQuestion("Donnez un indice pour le mot " +mot+ " :"));    //la lecture du mot marche pas
+        }
+    }
+    return indices
+}
