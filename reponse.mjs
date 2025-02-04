db79dd8790ae4bc8c0e179a66a9889e7b4826c35
Anais5s <anaisboisson@gmail.com>
2025-02-04 08:30:00 +0100

    mini correction

--- reponse.mjs
+++ reponse.mjs
@@ -3,6 +3,6 @@ import { askQuestion } from './selection_mot_mystere.mjs';
 export async function reponse(indices,i){
     console.log("A toi de deviner ",noms[i]," :");
     console.log("Voila tes indices:", indices);
-    rep= await askQuestion("Quelle est ta reponse?");
+    let rep= await askQuestion("Quelle est ta reponse?");
     return rep;
 }
